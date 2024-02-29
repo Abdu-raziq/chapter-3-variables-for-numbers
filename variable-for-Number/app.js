dda5be9a74fee877eda81040cb996d5bc338550f
Abdul Raziq <154526495+Abdu-raziq@users.noreply.github.com>
2024-02-29 22:23:05 +0500

    Add files via upload

---
+++ variable-for-Number/app.js
@@ -0,0 +1,14 @@
+let age=22;
+alert("I am " + age + " years old");
+
+let Visit_no = 14;
+alert("You have visited this site " + Visit_no + " times")
+
+let birth_year = 2002;
+document.write("My birth year is ", birth_year ,"<br/>","Datatype of my variables is number" ) 
+
+let Visitor_name = "Abdul Raziq";
+let Product_title = "T-Shirts";
+let Quantity = 5;
+
+document.write("<br/>",Visitor_name ," ordered " , Quantity , Product_title , " on AR-Garments");
